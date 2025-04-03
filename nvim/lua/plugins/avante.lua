7fca93f781312c43bfb6a55e24212db307174d82
Ian Pegg <ian.pegg@braincorp.com>
2025-04-03 12:50:40 -0700

    Fix avante config
    it is now sort of working, but we do get segfaults occasionally

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -7,6 +7,12 @@ return {
       -- add any opts here
       -- for example
       provider = "copilot",
+      file_selector = {
+        --- @alias FileSelectorProvider "native" | "fzf" | "mini.pick" | "snacks" | "telescope" | string | fun(params: avante.file_selector.IParams|nil): nil
+        provider = "fzf",
+        -- Options override for custom providers
+        provider_opts = {},
+      },
     },
     build = "make BUILD_FROM_SOURCE=true",
     -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
@@ -16,8 +22,9 @@ return {
       "nvim-lua/plenary.nvim",
       "MunifTanjim/nui.nvim",
       --- The below dependencies are optional,
-      "echasnovski/mini.pick", -- for file_selector provider mini.pick
-      "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+      -- "echasnovski/mini.pick", -- for file_selector provider mini.pick
+      -- "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+      "blink.compat", -- for file_selector provider blink-cmp
       "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
       "ibhagwan/fzf-lua", -- for file_selector provider fzf
       "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
@@ -49,20 +56,4 @@ return {
       },
     },
   },
-  -- {
-  --   "yetone/avante.nvim",
-  --   dependencies = {
-  --     "nvim-tree/nvim-web-devicons",
-  --     "stevearc/dressing.nvim",
-  --     "nvim-lua/plenary.nvim",
-  --     "MunifTanjim/nui.nvim",
-  --     {
-  --       "MeanderingProgrammer/render-markdown.nvim",
-  --       opts = { file_types = { "markdown", "Avante" } },
-  --       ft = { "markdown", "Avante" },
-  --     },
-  --   },
-  --   build = "make",
-  --   opts = { provider = "copilot" },
-  -- },
 }
