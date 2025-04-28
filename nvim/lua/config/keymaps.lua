40e14538f090edf314cf3bd2cf8116a3d03f816f
Ian Pegg <ian.pegg@braincorp.com>
2025-04-28 11:38:42 -0700

    nvim: update run menu

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,6 +2,13 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<leader>r", function()
+
+vim.keymap.set("n", "<leader>r", "<Nop>", { desc = "Run Menu" })
+
+vim.keymap.set("n", "<leader>rp", function()
   vim.cmd("wa | execute 'new | setlocal buftype=nofile | terminal python ' . expand('%')")
 end, { desc = "🐍 run file" })
+
+vim.keymap.set("n", "<leader>rt", function()
+  vim.cmd("wa | execute 'new | setlocal buftype=nofile | terminal pytest --pdb ' . expand('%')")
+end, { desc = "🐍 run pytest" })
