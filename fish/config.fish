920a0f24d985277450dfbb87869571a0d6a2c3a8
Ian Pegg <ian.pegg@braincorp.com>
2025-10-02 16:32:45 -0700

    fish: add conda

--- fish/config.fish
+++ fish/config.fish
@@ -4,3 +4,17 @@ set -g fish_greeting
 # activate a virtual env if there is one
 vs
 set_ti_pythonpath
+
+# conda initialize >>>
+set miniconda_home $HOME/miniconda3
+if test -f $miniconda_home/bin/conda
+    eval $miniconda_home/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "$miniconda_home/etc/fish/conf.d/conda.fish"
+        . "$miniconda_home/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "$miniconda_home/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
