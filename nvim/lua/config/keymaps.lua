2c80823edd774727fd3bc65daf7af375199cb982
Ian Pegg <ian.pegg@braincorp.com>
2025-10-21 10:40:03 -0700

    nvim: surround selection with brackets

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -24,3 +24,28 @@ vim.keymap.set("n", "<C-a>", "^", { desc = "Go to beginning of line (Emacs Ctrl+
 vim.keymap.set("n", "<C-e>", "$", { desc = "Go to end of line (Emacs Ctrl+e)" })
 vim.keymap.set("v", "<C-a>", "^", { desc = "Go to beginning of line (Emacs Ctrl+a)" })
 vim.keymap.set("v", "<C-e>", "$", { desc = "Go to end of line (Emacs Ctrl+e)" })
+
+local function surround_visual(open, close)
+  local keys = vim.api.nvim_replace_termcodes("c" .. open .. "<C-r>\"" .. close .. "<Esc>", true, true, true)
+  local was_disabled = vim.g.minipairs_disable
+  vim.g.minipairs_disable = true
+  vim.api.nvim_feedkeys(keys, "x", false)
+  vim.schedule(function()
+    vim.g.minipairs_disable = was_disabled
+  end)
+end
+
+for _, mapping in ipairs({
+  { '"', '"' },
+  { "'", "'" },
+  { "`", "`" },
+  { "(", ")" },
+  { "[", "]" },
+  { "{", "}" },
+  { "<", ">" },
+}) do
+  local open, close = mapping[1], mapping[2]
+  vim.keymap.set("x", open, function()
+    surround_visual(open, close)
+  end, { desc = ("Wrap selection with %s%s"):format(open, close) })
+end
