24bd66c6f908fa9666a17ce5b37f1ee37605b07d
Ian Pegg <ian.pegg@braincorp.com>
2026-03-31 15:42:27 -0700

    fish: fix `vs` in worktrees

--- fish/functions/vs.fish
+++ fish/functions/vs.fish
@@ -1,7 +1,28 @@
 function vs
-    if test -f venv/bin/activate
-        source venv/bin/activate.fish
-    else if test -f .venv/bin/activate
-        source .venv/bin/activate.fish
+    set -l roots $PWD
+
+    set -l gt (git rev-parse --show-toplevel 2>/dev/null)
+    set -l gt_status $status
+    if test $gt_status -eq 0; and test "$gt" != "$PWD"
+        set -a roots $gt
+    end
+
+    if test $gt_status -eq 0
+        set -l gc (git rev-parse --git-common-dir 2>/dev/null)
+        if test $status -eq 0
+            set -l main_root (path dirname $gc)
+            if test "$main_root" != "$PWD"; and test "$main_root" != "$gt"
+                set -a roots $main_root
+            end
+        end
+    end
+
+    for root in $roots
+        for dir in venv .venv
+            if test -f $root/$dir/bin/activate.fish
+                source $root/$dir/bin/activate.fish
+                return
+            end
+        end
     end
 end
